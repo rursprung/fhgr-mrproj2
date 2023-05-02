4df0e31c0f3e1676cd76474f4a39cd6ec0092366
Joel Flepp <joel_flepp@hotmail.com>
2023-05-02 18:29:56 +0200

    Delete custom signal handler

--- smt_movement_controller/src/smt_movement_controller_node.cpp
+++ smt_movement_controller/src/smt_movement_controller_node.cpp
@@ -1,19 +1,11 @@
-#include <pigpio.h>
+
 #include <ros/ros.h>
 
-#include "signal.h"
 #include "smt_movement_controller/movement_controller.hpp"
-
-void mySignitHandler(int sig) {
-    gpioTerminate();
-    ROS_INFO("mySignitHandler called!");
-    ros::shutdown();
-}
 
 int main(int argc, char **argv) {
     ros::init(argc, argv, "smt_movement_controller", ros::init_options::NoSigintHandler);
     ros::NodeHandle nodeHandle("~");
-    signal(SIGINT, mySignitHandler);
 
     smt::movement_controller::MovementController MovementController(nodeHandle);
 
