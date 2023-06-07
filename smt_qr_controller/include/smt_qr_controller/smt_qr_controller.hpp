4f3be8246947812627b4bb8d8243866fa590974e
Ralph Ursprung <ralph.ursprung@gmail.com>
2023-06-07 15:09:47 +0200

    QR code controller: turn towards QR code
    this is a very simplistic implementation as it does not reposition the
    robot and it does not consider the orientation of the QR code (which is
    currently also not being calculated by the QR code finder).
    
    open points:
    * we need to calculate the time for the delay between starting and
      stopping the rotation!

--- smt_qr_controller/include/smt_qr_controller/smt_qr_controller.hpp
+++ smt_qr_controller/include/smt_qr_controller/smt_qr_controller.hpp
@@ -3,6 +3,9 @@
 
 #include <ros/ros.h>
 #include <geometry_msgs/Pose.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+#include <tf2_ros/transform_listener.h>
 
 #include <smt_msgs/PoseWithString.h>
 
@@ -17,11 +20,17 @@ namespace smt {
         private:
             ros::Subscriber qrCodeSubscriber;
             ros::ServiceClient gunControllerClient;
+            ros::Publisher velocityCommandPublisher;
+
+            tf2_ros::Buffer tfBuffer;
+            tf2_ros::TransformListener tfListener;
 
             std::vector<std::string> handledQrCodes;
 
             bool qrCodeAlreadyHandled(std::string const& qrCode) const;
-            void handleQrCode(geometry_msgs::Pose const& qrCodePose);
+            void handleQrCode(geometry_msgs::PoseStamped const& qrCodePose);
+            void turnRobotByAngle(double turnAngle);
+            void fireGun(int gunAngle);
         };
     }
 }
--- smt_qr_controller/src/smt_qr_controller.cpp
+++ smt_qr_controller/src/smt_qr_controller.cpp
@@ -1,4 +1,7 @@
 #include <algorithm>
+#include <cmath>
+
+#include <geometry_msgs/Twist.h>
 
 #include <smt_svcs/FireGun.h>
 
@@ -7,10 +10,12 @@
 namespace smt {
     namespace qr_controller {
 
-        qr_controller::QrController::QrController(ros::NodeHandle &nodeHandle) {
+        qr_controller::QrController::QrController(ros::NodeHandle &nodeHandle) : tfListener(tfBuffer) {
             std::string qrCodeTopic;
             int qrCodeQueueSize;
             std::string gunControllerService;
+            std::string velTopic;
+            int velTopicQueueSize;
 
             if (!nodeHandle.getParam("qr_code_topic/topic", qrCodeTopic)) {
                 ROS_ERROR("failed to load the `gun_topic/topic` parameter!");
@@ -25,8 +30,18 @@ namespace smt {
                 ros::requestShutdown();
             }
 
+            if (!nodeHandle.getParam("cmd_vel_topic/topic", velTopic)) {
+                ROS_ERROR("failed to load the `cmd_vel_topic/topic` parameter!");
+                ros::requestShutdown();
+            }
+            if (!nodeHandle.getParam("cmd_vel_topic/queue_size", velTopicQueueSize)) {
+                ROS_ERROR("failed to load the `cmd_vel_topic/queue_size` parameter!");
+                ros::requestShutdown();
+            }
+
             this->qrCodeSubscriber = nodeHandle.subscribe(qrCodeTopic, qrCodeQueueSize, &QrController::qrCodeCallback, this);
             this->gunControllerClient = nodeHandle.serviceClient<smt_svcs::FireGun>(gunControllerService);
+            this->velocityCommandPublisher = nodeHandle.advertise<geometry_msgs::Twist>(velTopic, velTopicQueueSize)
         }
 
         void QrController::qrCodeCallback(smt_msgs::PoseWithString const& qrCode) {
@@ -34,7 +49,10 @@ namespace smt {
                 this->handledQrCodes.push_back(qrCode.text);
                 ROS_INFO_STREAM("received new QR code " << qrCode.text << " with pose " << qrCode.pose);
 
-                this->handleQrCode(qrCode.pose);
+                geometry_msgs::PoseStamped stampedQrCodePose;
+                stampedQrCodePose.header = qrCode.header;
+                stampedQrCodePose.pose = qrCode.pose;
+                this->handleQrCode(stampedQrCodePose);
             }
         }
 
@@ -42,10 +60,33 @@ namespace smt {
             return std::find(this->handledQrCodes.begin(), this->handledQrCodes.end(), qrCode) != this->handledQrCodes.end();
         }
 
-        void QrController::handleQrCode(const geometry_msgs::Pose &qrCodePose) {
-            // TODO: properly handle all of this
+        void QrController::handleQrCode(geometry_msgs::PoseStamped const& qrCodePose) {
+            geometry_msgs::TransformStamped const transformStamped = this->tfBuffer.lookupTransform("base_link", qrCodePose.header.frame_id, ros::Time(0));
+
+            geometry_msgs::PoseStamped qrCodePoseFromRobot;
+            tf2::doTransform(qrCodePose, qrCodePoseFromRobot, transformStamped);
+
+            auto const turnAngleInDegrees = std::atan2(qrCodePoseFromRobot.pose.position.y, qrCodePoseFromRobot.pose.position.x) * 180 / M_PI;
+            this->turnRobotByAngle(turnAngleInDegrees);
+
+            // we currently don't calculate the height of the QR code
+            this->fireGun(10);
+        }
+
+        void QrController::turnRobotByAngle(const double turnAngle) {
+            // for now this is pretty simple:
+            // we turn at a fixed velocity and calculate the time we need to give it until we stop the rotation again.
+            geometry_msgs::Twist twistMessage;
+            twistMessage.angular.z = 2;
+            this->velocityCommandPublisher.publish(twistMessage);
+            ros::Duration(2).sleep(); // FIXME: calculate time based on turn angle!
+            twistMessage.angular.z = 0;
+            this->velocityCommandPublisher.publish(twistMessage);
+        }
+
+        void QrController::fireGun(const int gunAngle) {
             smt_svcs::FireGun fireGunCall;
-            fireGunCall.request.angle = 10;
+            fireGunCall.request.angle = gunAngle;
             if (this->gunControllerClient.call(fireGunCall)) {
                 ROS_INFO("successfully fired gun");
             } else {
